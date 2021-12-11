fc14237074664811550b1793f5ccf9935b2e5ec7
Tsimur Hadeliya <tsimur.hadeliya@allegro.pl>
2021-12-11 21:15:17 +0100

    Apply old changes in dataset file

--- dpr/dataset.py
+++ dpr/dataset.py
@@ -5,45 +5,30 @@ from collections import namedtuple
 import torch
 from torch.utils.data import Dataset
 
-QAPair = namedtuple("QAPair", ["question", "pair"])
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+QAPair = namedtuple("QAPair", ["question", "answer"])
+QAPairBatch = namedtuple("QAPair", ["question_list", "pair_list"])
 
-@dataclass
-class DPRInput:
-    questions: T.List
-    passages: T.List
-
-
-@dataclass
-class DPRRetrieverInput:
-    questions: T.List[str]
-    answers: T.List[str]
-
-
-@dataclass
-class DPRReaderInput:
-    pass
-
-
-@dataclass
-class DPRReaderInput:
-    pass
 
 class PolEvalQA(Dataset):
     def __init__(self):
-        answer_path = "/Users/tsimur.hadeliya/code/repos/DRP/data/PolEvalQA/dev-0/expected.tsv"
-        question_path = "/Users/tsimur.hadeliya/code/repos/DRP/data/PolEvalQA/dev-0/in.tsv"
+        answer_path = (
+            "/Users/tsimur.hadeliya/code/repos/DRP/data/PolEvalQA/dev-0/expected.tsv"
+        )
+        question_path = (
+            "/Users/tsimur.hadeliya/code/repos/DRP/data/PolEvalQA/dev-0/in.tsv"
+        )
         self.question = self.read_files(question_path)
         self.answer = self.read_files(answer_path)
         assert len(self.question) == len(self.answer)
-        self.len = len(self.question)
+        self.num_pairs = len(self.question)
 
     @staticmethod
-    def read_files(path):
+    def read_files(path: str):
         return open(path).read().splitlines()
 
-    def __getitem__(self, idx):
-        return QAPair(
-            question=self.question[idx],
-            answer=self.answer[idx]
-        )
+    def __getitem__(self, idx: int):
+        return QAPair(question=self.question[idx], answer=self.answer[idx])
+
+    def __len__(self):
+        return self.num_pairs
